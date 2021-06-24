82f0761ff13adf4f4bacc7d85b7411de2caae963
deven367 <31466137+deven367@users.noreply.github.com>
2021-06-24 00:07:19 -0400

    add new functions

--- clean_plot/_nbdev.py
+++ clean_plot/_nbdev.py
@@ -3,9 +3,15 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"say_hello": "00_core.ipynb",
-         "HelloSayer": "00_core.ipynb"}
+         "HelloSayer": "00_core.ipynb",
+         "get_data": "01_trial.ipynb",
+         "normalize": "03_functions.ipynb",
+         "split_by_newline": "03_functions.ipynb",
+         "make_sentences": "03_functions.ipynb"}
 
-modules = ["core.py"]
+modules = ["core.py",
+           "trial.py",
+           "functions.py"]
 
 doc_url = "https://deven367.github.io/clean_plot/"
 
