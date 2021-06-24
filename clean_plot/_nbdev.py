24e26c0636562efa7bb073fe9dccbce11c36adfe
deven367 <31466137+deven367@users.noreply.github.com>
2021-06-24 09:30:40 -0400

    change comment from exports to export

--- clean_plot/_nbdev.py
+++ clean_plot/_nbdev.py
@@ -5,12 +5,14 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"say_hello": "00_core.ipynb",
          "HelloSayer": "00_core.ipynb",
          "get_data": "01_trial.ipynb",
+         "get_features": "02_feature_extractor.ipynb",
          "normalize": "03_functions.ipynb",
          "split_by_newline": "03_functions.ipynb",
          "make_sentences": "03_functions.ipynb"}
 
 modules = ["core.py",
            "trial.py",
+           "feature_extractor.py",
            "functions.py"]
 
 doc_url = "https://deven367.github.io/clean_plot/"
