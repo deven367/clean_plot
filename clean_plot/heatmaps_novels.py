425863bd23f7529869c8399513d8b170aa14ff7a
deven367 <masterdeven@gmail.com>
2022-02-28 21:04:53 -0500

    add function for standardized ssms

--- clean_plot/heatmaps_novels.py
+++ clean_plot/heatmaps_novels.py
@@ -202,3 +202,76 @@ def lex_ts(path: Param("path for embeddings")):
         np.save(f'{f.stem[:-3]}ts', z)
         print(len(z))
 
+# Cell
+@call_parse
+def plot_standardized(path: Param("path for embeddings"),
+                start: Param("start for section", default=0, type=int),
+                end: Param("end for section", default=-1, type=int),
+                x: Param("x-ticks", default=50, type=int),
+                y: Param("y-ticks", default=50, type=int)):
+    "Generates plots for embeddings in the folder"
+
+    if start > end:
+        assert 'Incorrect bounds'
+
+    # Marker for xticks and yticks
+    if x == -1:
+        x = False
+    if y == -1:
+        y = False
+
+    files = loader(path, '.npy')
+    curr = Path.cwd()
+    if start > 0:
+        new_path = curr/f'sections_{start} {end}'
+        new_path.mkdir(exist_ok=True)
+    else:
+        new_path = curr/'full_plots'
+        new_path.mkdir(exist_ok=True)
+
+    for f in files:
+        fname = f.stem.split('_cleaned_')
+        book, method = fname[0], label(fname[1])
+
+        title = f'{book.title()} {method}'
+
+        em = np.load(f)
+
+        if start == 0:
+            start = 1
+
+        if end == -1:
+            end = len(em)
+
+
+        ticks = np.linspace(1, end - start, 5, dtype=int)
+        labels = np.linspace(start, end, 5, dtype=int)
+
+        if fname[1] == 'lexical_wt_ssm':
+            sim = em
+            print(em.shape)
+            n = normalize(sim)
+            np.fill_diagonal(sim, 1)
+        else:
+            sim = cosine_similarity(em, em)
+            n = normalize(sim)
+
+
+        numerator = n - np.mean(n)
+        denominator = np.sqrt(np.sum(numerator**2) / (numerator.size - 1) )
+
+        ab1 = numerator / denominator
+
+        sns.heatmap(ab1[start:end, start:end], cmap='hot',
+                    vmin=0, vmax=1, square=True,
+                    xticklabels=False)
+
+
+        plt.yticks(ticks, labels, rotation = 0)
+#         plt.title(title)
+        plt.ylabel('sentence number')
+        plt.savefig(new_path/f'{title}.png', dpi = 300, bbox_inches='tight')
+        plt.savefig(new_path/f'{title}.pdf', dpi = 300, bbox_inches='tight')
+        print(f'Done plotting {title}')
+        plt.clf()
+        del em, sim, n, numerator, denominator, ab1
