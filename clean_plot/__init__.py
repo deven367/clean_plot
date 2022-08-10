9d5eee43a75615062e41fa6c46513ec5877bd386
deven367 <masterdeven@gmail.com>
2022-08-09 21:05:51 -0400

    add logic to download nltk packages

--- clean_plot/__init__.py
+++ clean_plot/__init__.py
@@ -1,3 +1,8 @@
 __version__ = "0.0.12"
 from .utils import download_nltk_dep
-download_nltk_dep()
+try:
+    from nltk.corpus import stopwords
+    STOPWORDS = set(stopwords.words('english'))
+except:
+    print('Downloading dependencies')
+    download_nltk_dep()
