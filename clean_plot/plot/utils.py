4a9c4bc11b8c27dc9c04c34b6960a8ac069b268c
deven367 <masterdeven@gmail.com>
2022-08-14 22:38:53 -0400

    update docments

--- clean_plot/plot/utils.py
+++ clean_plot/plot/utils.py
@@ -24,10 +24,13 @@ import inspect
 
 # %% ../nbs/04_plot.utils.ipynb 6
 class Plot:
-    def __init__(self, path):
+    "Plotting module"
+    def __init__(self, 
+                 path: str, # path to embeddings
+                ):
         self.path = Path(path)
         self.norm = {}
-        self.book_name = self.path.stem.replace('_', ' ')
+        self.book_name = self.path.stem.split('_cleaned')[0].replace('_', ' ').title()
         
     @delegates(globtastic)
     def view_all_files(self, **kwargs):
@@ -59,7 +62,10 @@ class Plot:
     def get_corr_plots(self):
         pass
     
-    def get_sectional_ssms(self, start, end):
+    def get_sectional_ssms(self, 
+                           start, # start of the cross section 
+                           end, # end of the cross section
+                          )->None:
         import gc
         if start == 0 and end == -1:
             pass
@@ -94,8 +100,8 @@ class Plot:
     
     def __repr__(self):
         # remember __str__ calls the __repr_ internally
-        dir_path = os.path.dirname(os.path.realpath(self.path))
-        return f'This object contains the path to `{dir_path}`'
+#         dir_path = os.path.dirname(os.path.realpath(self.path))
+        return f'This object contains the path to `{self.path.absolute()}`'
 
 # %% ../nbs/04_plot.utils.ipynb 7
 @patch
@@ -121,7 +127,8 @@ def get_normalized(self:Plot):
         else:
             sim = cosine_similarity(em, em)
             n = normalize(sim)
-        
+            
+#         yield method, n
         self.norm[method] = n
         del em, sim, n
     return self.norm
